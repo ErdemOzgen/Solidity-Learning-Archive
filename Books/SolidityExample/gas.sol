7681d56a7d725c30fbccd2532ad496d2bf201794
ErdemOzgen <m.erdemozgen@gmail.com>
2022-02-03 18:09:33 +0300

    add br in gas comment

--- Books/SolidityExample/gas.sol
+++ Books/SolidityExample/gas.sol
@@ -15,6 +15,7 @@ There are 2 upper bounds to the amount of gas you can spend
 
 gas limit (max amount of gas you're willing to use for your transaction, set by you)
 block gas limit (max amount of gas allowed in a block, set by the network)
+
 */
 contract Gas {
     uint public i = 0;
