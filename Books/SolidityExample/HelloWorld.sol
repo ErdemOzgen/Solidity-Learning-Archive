32ccbaf2521971390190ba9105b63cf21aaf0a24
ErdemOzgen <m.erdemozgen@gmail.com>
2022-02-03 15:39:43 +0300

    add hello

--- Books/SolidityExample/HelloWorld.sol
+++ Books/SolidityExample/HelloWorld.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
-// compiler version must be greater than or equal to 0.8.3 and less than 0.9.0
-pragma solidity ^0.8.3;
-
+// compiler version must be greater than or equal to 0.8.11 and less than 0.9.0
+pragma solidity ^0.8.10;
+/// @title ERC20Basic
+/// @dev Simpler version of ERC20 interface
+/// @author ErdemOzgen
 contract HelloWorld {
     string public greet = "Hello World!";
 }
