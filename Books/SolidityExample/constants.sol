62c768a4477695220c63cbf036d9503be9683e90
ErdemOzgen <m.erdemozgen@gmail.com>
2022-02-03 17:28:40 +0300

    add constants

--- Books/SolidityExample/constants.sol
+++ Books/SolidityExample/constants.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
-
+//Their value is hard coded and using constants can save gas cost.
 contract Constants {
     // coding convention to uppercase constant variables
     address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
