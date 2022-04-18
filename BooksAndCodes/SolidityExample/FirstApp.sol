97a54bc06b06e080c778947ae718cc80f08eff79
ErdemOzgen <m.erdemozgen@gmail.com>
2022-04-19 00:21:46 +0300

    add links for function states

--- BooksAndCodes/SolidityExample/FirstApp.sol
+++ BooksAndCodes/SolidityExample/FirstApp.sol
@@ -5,6 +5,28 @@ contract Counter {
     uint public count;
 
     // Function to get the current count
+    // For Function State look that link ==> 
+    //https://medium.com/coinmonks/function-state-mutability-in-solidity-acb850eedccc
+    /**
+    The two major state mutability modifiers in solidity are:
+        * View
+        * Pure
+
+        View functions are read-only functions and are declared 
+        with the view keyword. They do not modify the state of 
+        the blockchain. This means that functions declared with 
+        the view keyword cannot contain code that potentially modify
+        state variables or data stored on the blockchain.Additionally,
+        view functions cannot  receive or send ether and can only call other view or pure
+        functions
+
+        Functions marked as pure neither read from nor write to the blockchain. 
+        They cannot receive or send ether, cannot use members of msg or
+        members of block and can only call other pure functions
+
+
+    
+     */
     function get() public view returns (uint) {
         return count;
     }
--- BooksAndCodes/SolidityExample/HelloWorld.sol
+++ BooksAndCodes/SolidityExample/HelloWorld.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// compiler version must be greater than or equal to 0.8.11 and less than 0.9.0
+// compiler version must be greater than or equal to 0.8.10 and less than 0.9.0
 pragma solidity ^0.8.10;
 /// @title ERC20Basic
 /// @dev Simpler version of ERC20 interface
