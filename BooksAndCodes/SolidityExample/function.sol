d6e3c3dae36e200574dbd4bd96ab2b50ecee00d3
ErdemOzgen <m.erdemozgen@gmail.com>
2022-04-19 00:24:46 +0300

    add function visibility

--- BooksAndCodes/SolidityExample/function.sol
+++ BooksAndCodes/SolidityExample/function.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
-
+// Function visibility:
+// https://bitsofco.de/solidity-function-visibility-explained/
 contract Function {
     // Functions can return multiple values.
     function returnMany()
