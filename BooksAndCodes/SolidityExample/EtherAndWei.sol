b41fade1c233e8ebd663a1f93046c206319dea92
ErdemOzgen <m.erdemozgen@gmail.com>
2022-05-05 17:04:17 +0300

    add gwei in comments

--- BooksAndCodes/SolidityExample/EtherAndWei.sol
+++ BooksAndCodes/SolidityExample/EtherAndWei.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 //Similar to how one dollar is equal to 100 cent, one ether is equal to 1018 wei.
+// 1ETH = 1Ether = 1E18 wei = 1000000000 Gwei
+
 contract EtherUnits {
     uint public oneWei = 1 wei;
     // 1 wei is equal to 1
