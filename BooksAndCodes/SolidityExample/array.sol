cbeb92a6377ad7ea3b64923c3c1d6968d0ede425
ErdemOzgen <m.erdemozgen@gmail.com>
2022-05-05 17:32:59 +0300

    add memory explained commet

--- BooksAndCodes/SolidityExample/array.sol
+++ BooksAndCodes/SolidityExample/array.sol
@@ -18,7 +18,23 @@ contract Array {
     function getArr() public view returns (uint[] memory) {
         return arr;
     }
+//memory tells solidity to create a chunk of space for the variable at method runtime, guaranteeing its size and structure for future use in that method.
+/*
+The Ethereum Virtual Machine has three areas where it can store items.
 
+The first is “storage”, where all the contract state variables reside. Every contract has its own storage and it is persistent between function calls and quite expensive to use.
+
+The second is “memory”, this is used to hold temporary values. It is erased between (external) function calls and is cheaper to use.
+
+The third one is the stack, which is used to hold small local variables. It is almost free to use, but can only hold a limited amount of values.
+
+For almost all types, you cannot specify where they should be stored, because they are copied everytime they are used.
+
+The types where the so-called storage location is important are structs and arrays. If you e.g. pass such variables in function calls, their data is not copied if it can stay in memory or stay in storage. This means that you can modify their content in the called function and these modifications will still be visible in the caller.
+
+
+ */
+memory cannot be used at the contract level. Only in methods.
     function push(uint i) public {
         // Append to array
         // This will increase the array length by 1.
